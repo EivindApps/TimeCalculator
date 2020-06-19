884ad8eb39523446195e7cc41d280995ffb85c08
Eivind Gussiås Løkseth <eloekset@msn.com>
2020-06-19 23:14:08 +0200

    Add basic calculator UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Calculator">
+      <div className="CalculatorDisplayContainer">
+        <input type="text" className="CurrentCalculation" disabled value="05:50 + 03:30 +"></input>
+        <input type="text" className="CalculatorDisplay" disabled value="09:20"></input>
+      </div>
+      <div className="CalculatorPad">
+        <div className="CalculatorButton">CE</div>
+        <div className="CalculatorButton">C</div>
+        <div className="CalculatorButton">&#x21A4;</div>
+        <div className="NoCalculatorButton"></div>
+        <div className="CalculatorButton">7</div>
+        <div className="CalculatorButton">8</div>
+        <div className="CalculatorButton">9</div>
+        <div className="NoCalculatorButton"></div>
+        <div className="CalculatorButton">4</div>
+        <div className="CalculatorButton">5</div>
+        <div className="CalculatorButton">6</div>
+        <div className="CalculatorButton">&#x2212;</div>
+        <div className="CalculatorButton">1</div>
+        <div className="CalculatorButton">2</div>
+        <div className="CalculatorButton">3</div>        
+        <div className="CalculatorButton">+</div>
+        <div className="CalculatorButton">&#x2213;</div>
+        <div className="CalculatorButton">0</div>        
+        <div className="CalculatorButton">:</div>
+        <div className="CalculatorButton">=</div>
+      </div>
     </div>
   );
 }
